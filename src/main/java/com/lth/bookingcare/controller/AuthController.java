a1a027fda9886cc2beb21056c978c7f7832763a4
Hung Le <hunglehung812@gmail.com>
2024-09-04 15:16:36 +0700

    major contatc auth

---
+++ src/main/java/com/lth/bookingcare/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.lth.bookingcare.controller;
+
+import com.lth.bookingcare.request.LoginRequest;
+import com.lth.bookingcare.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import response.ErrorResponse;
+import response.SuccessResponse;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            Map<String, String> u = userService.login(loginRequest);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new SuccessResponse<>("Login successfully ! "));
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse<>("Server error !"));
+        }
+    }
+}
