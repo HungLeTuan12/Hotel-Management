ee196843e29b45a2c9a30999982026a7b8faae56
Hung Le <hunglehung812@gmail.com>
2024-09-05 09:54:04 +0700

    respone DTO

--- src/main/java/com/lth/hotelmanagement/response/BookingResponse.java
+++ src/main/java/com/lth/hotelmanagement/response/BookingResponse.java
@@ -1,4 +1,28 @@
 package com.lth.hotelmanagement.response;
 
+import com.lth.hotelmanagement.entity.Room;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class BookingResponse {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmCode;
+    private RoomResponse room;
+
+    // Constructor
+
+    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmCode = bookingConfirmCode;
+    }
 }
--- src/main/java/com/lth/hotelmanagement/response/RoomResponse.java
+++ src/main/java/com/lth/hotelmanagement/response/RoomResponse.java
@@ -1,4 +1,34 @@
 package com.lth.hotelmanagement.response;
 
+import com.lth.hotelmanagement.entity.BookedRoom;
+import jakarta.persistence.*;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomResponse {
+    private Long id;
+    private String roomType;
+    private BigDecimal price;
+    private boolean isBooked = false;
+    private String photo;
+    private List<BookingResponse> bookings = new ArrayList<>();
+    // Constructor
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.price = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal price, boolean isBooked, byte[] photo, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.price = price;
+        this.isBooked = isBooked;
+        this.photo = photo != null ? Base64.encodeBase64String(photo) : null;
+        this.bookings = bookings;
+    }
 }
